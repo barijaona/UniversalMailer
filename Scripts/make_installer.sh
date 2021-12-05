214da536155e78355d8f472dd9aafd0ca7c19a8f
Barijaona Ramaholimihaso <github.com@barijaona.com>
2021-12-05 17:55:32 +0300

    Clarify how installer generation must be made
    Fail explicitly if installer generation is run with Debug configuration

--- Scripts/make_installer.sh
+++ Scripts/make_installer.sh
@@ -2,9 +2,11 @@
 
 # Script for creating installer package
 
-if [[ $CONFIGURATION != "Debug" ]]; then
+# Fail if not deployment
+if [ ! "${CONFIGURATION}" = "Release" ]; then
+    echo "error: This should only be run as Release ; use 'Build For Profiling' in Xcode" >&2
+    exit 1
+fi
+
 pkgbuild --root "$BUILT_PRODUCTS_DIR/UniversalMailer.mailbundle/" --scripts "$SRCROOT/Scripts/installation_scripts/" --identifier it.noware.universalmailer.pkg --install-location "/tmp/UM-installation-tmp/UniversalMailer.mailbundle" "$BUILT_PRODUCTS_DIR/UniversalMailer.pkg"
 open "$BUILT_PRODUCTS_DIR"
-else
-echo "warning: installer not generated with Debug configuration"
-fi
