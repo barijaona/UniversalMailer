1c4a5904c6d65ea59036c8ba190c4470667cd366
Barijaona Ramaholimihaso <github.com@barijaona.com>
2021-12-12 06:42:07 +0300

    Resign Sparkle framework and bundle
    cf. https://furbo.org/2019/08/16/catalina-app-notarization-and-sparkle/

--- Scripts/make_installer.sh
+++ Scripts/make_installer.sh
@@ -8,5 +8,13 @@ if [ ! "${CONFIGURATION}" = "Release" ]; then
     exit 1
 fi
 
+CODE_SIGN_IDENTITY="Developer ID Application: Barijaona Ramaholimihaso"
+frameworks_path="$BUILT_PRODUCTS_DIR/UniversalMailer.mailbundle/Contents/Frameworks"
+
+# cf. https://furbo.org/2019/08/16/catalina-app-notarization-and-sparkle/
+codesign --verbose --force --deep -o runtime --sign "${CODE_SIGN_IDENTITY}" "$frameworks_path/Sparkle.framework/Versions/A/Resources/AutoUpdate.app"
+codesign --verbose --force -o runtime --sign "${CODE_SIGN_IDENTITY}" "$frameworks_path/Sparkle.framework/Versions/A"
+codesign --verbose --force -o runtime --sign "${CODE_SIGN_IDENTITY}" "$BUILT_PRODUCTS_DIR/UniversalMailer.mailbundle"
+
 pkgbuild --root "$BUILT_PRODUCTS_DIR/UniversalMailer.mailbundle/" --scripts "$SRCROOT/Scripts/installation_scripts/" --identifier it.noware.universalmailer.pkg --install-location "/tmp/UM-installation-tmp/UniversalMailer.mailbundle" "$BUILT_PRODUCTS_DIR/UniversalMailer.pkg"
 open "$BUILT_PRODUCTS_DIR"
