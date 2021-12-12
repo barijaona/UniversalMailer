3214d570150c1bae5fbb1f3212da712c9d229729
Barijaona Ramaholimihaso <github.com@barijaona.com>
2021-12-12 07:47:34 +0300

    Sign the package

--- Scripts/make_installer.sh
+++ Scripts/make_installer.sh
@@ -9,6 +9,7 @@ if [ ! "${CONFIGURATION}" = "Release" ]; then
 fi
 
 CODE_SIGN_IDENTITY="Developer ID Application: Barijaona Ramaholimihaso"
+PKG_SIGN_IDENTITY="Developer ID Installer: Barijaona Ramaholimihaso"
 frameworks_path="$BUILT_PRODUCTS_DIR/UniversalMailer.mailbundle/Contents/Frameworks"
 
 # cf. https://furbo.org/2019/08/16/catalina-app-notarization-and-sparkle/
@@ -16,5 +17,9 @@ codesign --verbose --force --deep -o runtime --sign "${CODE_SIGN_IDENTITY}" "$fr
 codesign --verbose --force -o runtime --sign "${CODE_SIGN_IDENTITY}" "$frameworks_path/Sparkle.framework/Versions/A"
 codesign --verbose --force -o runtime --sign "${CODE_SIGN_IDENTITY}" "$BUILT_PRODUCTS_DIR/UniversalMailer.mailbundle"
 
-pkgbuild --root "$BUILT_PRODUCTS_DIR/UniversalMailer.mailbundle/" --scripts "$SRCROOT/Scripts/installation_scripts/" --identifier it.noware.universalmailer.pkg --install-location "/tmp/UM-installation-tmp/UniversalMailer.mailbundle" "$BUILT_PRODUCTS_DIR/UniversalMailer.pkg"
+pkgbuild --root "$BUILT_PRODUCTS_DIR/UniversalMailer.mailbundle/" --scripts "$SRCROOT/Scripts/installation_scripts/" \
+         --identifier it.noware.universalmailer.pkg --install-location "/tmp/UM-installation-tmp/UniversalMailer.mailbundle" \
+         --sign "${PKG_SIGN_IDENTITY}" \
+         "$BUILT_PRODUCTS_DIR/UniversalMailer.pkg"
+
 open "$BUILT_PRODUCTS_DIR"
